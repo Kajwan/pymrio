0f12d749acfeb2c4bc74db9f0801afa88f410989
Kajwan <kajwanr@gmail.com>
2025-07-16 15:27:39 +0200

    added more documentation

--- pymrio/core/mriosystem.py
+++ pymrio/core/mriosystem.py
@@ -3366,16 +3366,68 @@ class IOSystem(_BaseSystem):
         multipliers=True,
         downstream_allocation_matrix="A12",
         save_extraction=True,
-        save_path="./test_extraction",
+        save_path="./HEM_results",
         calculate_impacts=True,
-        impact_account="all",
+        extension="all",
         specific_impact=None,  # If specific impact is provided, details on other sectors are kept.
         save_impacts=True,
         save_core_IO=True,
         save_details=True,
         return_results=False,
     ):
-        """Apply the HEM method to the IOSystem."""
+        """Apply a specific extraction using the HEM to the IOSystem.
+
+        Parameters
+        ----------
+        regions : list, optional
+            List of regions to consider in the HEM. If None, all regions are considered.
+            Default: None
+        sectors : list, optional
+            List of sectors to consider in the HEM. If None, all sectors are considered.
+            Default: None
+        extraction_type : str, optional
+            Type of extraction to apply. See "https://doi.org/10.1111/jiec.13522" for more information.
+            Default: "1.2"
+        multipliers : bool, optional
+            Whether to calculate HEM multipliers . Default: True
+        downstream_allocation_matrix : str, optional
+            The allocation matrix to use for downstream allocation. Can be "A12" or "L12".
+            Default: "A12"
+        save_extraction : bool, optional
+            Whether to save the extraction results to disk. Default: True
+        save_path : str, optional
+            Path to save the extraction results. Default: "./HEM_results"
+        calculate_impacts : bool, optional
+            Whether to calculate environmental impacts. Default: True
+        extension : str, optional
+            The extension to use for impact calculations. Can be "all" or a specific extension name.
+            Default: "all"
+        specific_impact : str, optional
+            If provided, only this specific impact will be calculated.
+            If None, all impacts in extensions are calculated.
+            Default: None
+        save_impacts : bool, optional
+            Whether to save the impact results to disk. Default: True
+        save_core_IO : bool, optional
+            Whether to save the core IO results along with the extraction. Default: True
+        save_details : bool, optional
+            Whether to save detailed results of the extraction. Default: True
+        return_results : bool, optional
+            Whether to return the HEM results as a list. Default: False
+
+        Returns
+        -------
+        list
+            If return_results is True, returns a list of HEM results. Otherwise, returns None.
+
+        Raises
+        ------
+        ValueError
+            If neither regions nor sectors are specified.
+        ValueError
+            If specific_impact is provided but extension is "all".
+
+        """
         # TODO: Option to whether or not add results as an attribute in PyMRIO object.
 
         if (regions is None) & (sectors is None):
@@ -3403,9 +3455,9 @@ class IOSystem(_BaseSystem):
         HEM_results = []
 
         if calculate_impacts:
-            if impact_account == "all":
+            if extension == "all":
                 if specific_impact is not None:
-                    raise ValueError("If specific_impact is given, impact_account must not be 'all'.")
+                    raise ValueError("If specific_impact is given, extension must not be 'all'.")
                 for impact in self.get_extensions():
                     impact_extension = getattr(self, impact)
 
@@ -3418,7 +3470,7 @@ class IOSystem(_BaseSystem):
                     else:
                         HEM_results.append(HEM_object)
             else:
-                impact_extension = getattr(self, impact_account)
+                impact_extension = getattr(self, extension)
                 if impact_extension.S is None:
                     impact_extension.S = calc_S(impact_extension.F, self.x)
 
@@ -3428,7 +3480,7 @@ class IOSystem(_BaseSystem):
                     HEM_object.calculate_impacts(intensities=impact_extension.S.loc[specific_impact, :])
 
                 if save_impacts:
-                    HEM_object.save_impacts(impact_account=impact_account, specific_impact=specific_impact)
+                    HEM_object.save_impacts(impact_account=extension, specific_impact=specific_impact)
                 if return_results:
                     HEM_results.append(HEM_object)
             return HEM_results
